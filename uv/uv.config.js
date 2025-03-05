75bd27e689f5a1be9a1aee809479653f4a78c751
ubg-hub-v3 <math.trigonometry.alt.291@gmail.com>
2025-03-04 21:20:44 -0600

    updates

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@ self.__uv$config = {
     prefix: '/service/',
 
     /* Bare server URL */ 
-    bare: 'https://tomp.app',
+    bare: 'https://bare.benrogo.net/',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
